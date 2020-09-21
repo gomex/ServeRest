10c8bf96e1643622fe32dd2dc4da15f537744fcc
Paulo Gonçalves <paulorochag@hotmail.com>
2020-09-21 00:56:41 -0300

    test: deploy

--- src/utils/monitor.js
+++ src/utils/monitor.js
@@ -8,7 +8,7 @@ Esse arquivo está marcado para ser ignorado no arquivo stryker.conf.js
 */
 
 const moesif = require('moesif-nodejs')
-const os = require('os')
+// const os = require('os')
 
 const { version: serverestVersion } = require('../../package.json')
 
@@ -16,7 +16,7 @@ const ehAmbienteDeDesenvolvimento = process.env.NODE_ENV === 'serverest-developm
 const ehAmbienteDeTestes = process.env.NODE_ENV === 'serverest-test'
 const moesifMiddleware = moesif({
   applicationId: 'eyJhcHAiOiIxNTA6MTU1MCIsInZlciI6IjIuMCIsIm9yZyI6IjQ5MToxMTIxIiwiaWF0IjoxNTk4OTE4NDAwfQ.e0E6Qhz1o1Jjs5prulHDYEBlv0juruWs_btjq2mong8',
-  identifyUser: (req, res) => { return os.userInfo().username },
+  identifyUser: (req, res) => { return 'web' },
   identifyCompany: (req, res) => { return serverestVersion }
 })
 
